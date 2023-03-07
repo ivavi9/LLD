3ae7ddf0fa1bc5c72611aa627a7cc6dd03dc5ef1
ivavi9 <25426909+ivavi9@users.noreply.github.com>
2023-03-07 19:35:27 +0530

    SOLID+Design Pattern factory addition

--- src/main/java/observer/Client.java
+++ src/main/java/observer/Client.java
@@ -1,11 +1,15 @@
 package observer;
 
+
+
 import java.util.List;
 
 public class Client {
     public static void main(String[] args) {
         Amazon amazon = new Amazon();
         GenerateInvoice generateInvoice = new GenerateInvoice();
+        InventoryManagement inventoryManagement = new InventoryManagement();
+        ShipOrder shipOrder = new ShipOrder();
         
         amazon.createOrder(List.of("Something","Order","Avi"));
 
